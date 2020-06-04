4ca04239898a1869c6c8645ef8e7680acc8a0f5f
Yash Chaudhari <yash56244@gmail.com>
2020-06-04 13:48:47 +0530

    added mail support

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -13,5 +15,12 @@ migrate=Migrate(app, db)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view = 'login'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = "email"
+app.config['MAIL_PASSWORD'] = "password"
+mail = Mail(app)
 
 from main import routes
